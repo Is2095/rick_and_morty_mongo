3317e1eaf0ba2d2090fc5e93cf0abbcf4188d911
Ismael <ismael2095951@gmail.com>
2023-08-12 22:10:57 -0300

    deploy

--- cliente/src/components/SearchBar/SearchBar.jsx
+++ cliente/src/components/SearchBar/SearchBar.jsx
@@ -95,6 +95,7 @@ const SearchBar = () => {
    return (
       <div>
 
+<h1>{process.env.REACT_APP_LOCAL}</h1>
          <Modal2 cierreModal={cierreModal} /> 
          {/* {(cartelModal.permiso) ? <Modal permiso={cartelModal.permiso} texto={cartelModal.texto} cierreModal={cierreModal} /> : null} */}
          <div>
