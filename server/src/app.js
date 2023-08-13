9286ddffddec6c60454dce14d68ea8e6fbf6b007
Ismael <ismael2095951@gmail.com>
2023-08-12 23:19:58 -0300

    deploy

--- server/src/app.js
+++ server/src/app.js
@@ -5,24 +5,30 @@ const router = require('./routes/index')
 const cors = require("cors");
 
 //const PORT = 3001;
-server.use(cors())
+server.use(cors({
+   origin: "*",
+   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+   allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 
-server.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', '*');
-   res.header('Access-Control-Allow-Credentials', 'true');
-   res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-      );
-      res.header(
-         'Access-Control-Allow-Methods',
-         'GET, POST, OPTIONS, PUT, DELETE'
-      );
-      res.header("X-Total-Count", "1000")
-      next();
-      });    
+// server.use((req, res, next) => {
+//    // res.header('Access-Control-Allow-Origin', '*');
+//    // res.header('Access-Control-Allow-Credentials', 'true');
+//    // res.header(
+//    //    'Access-Control-Allow-Headers',
+//    //    'Origin, X-Requested-With, Content-Type, Accept'
+//    //    );
+//       // res.header(
+//       //    'Access-Control-Allow-Methods',
+//       //    'GET, POST, OPTIONS, PUT, DELETE'
+//       // );
+//       // res.header("X-Total-Count", "1000")
+
+
+//       next();
+//       });    
       
 server.use("/rickandmorty", router)
 
