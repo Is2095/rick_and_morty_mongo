48e0df0da92112a93d934304cafd5efb69e9eb02
Ismael <ismael2095951@gmail.com>
2023-08-14 15:18:58 -0300

    deploy

--- server/src/app.js
+++ server/src/app.js
@@ -5,30 +5,30 @@ const router = require('./routes/index')
 const cors = require("cors");
 
 //const PORT = 3001;
-server.use(cors({
-   origin: "*",
-   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-   allowedHeaders: ['Content-Type', 'Authorization'],
-}))
+// server.use(cors({
+//    origin: "*",
+//    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//    allowedHeaders: ['Content-Type', 'Authorization'],
+// }))
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 
-// server.use((req, res, next) => {
-//    // res.header('Access-Control-Allow-Origin', '*');
-//    // res.header('Access-Control-Allow-Credentials', 'true');
-//    // res.header(
-//    //    'Access-Control-Allow-Headers',
-//    //    'Origin, X-Requested-With, Content-Type, Accept'
-//    //    );
-//       // res.header(
-//       //    'Access-Control-Allow-Methods',
-//       //    'GET, POST, OPTIONS, PUT, DELETE'
-//       // );
-//       // res.header("X-Total-Count", "1000")
+server.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', 'https://rick-and-morty-mongo-front.vercel.app');
+   res.header('Access-Control-Allow-Credentials', 'true');
+   res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+      );
+      res.header(
+         'Access-Control-Allow-Methods',
+         'GET, POST, OPTIONS, PUT, DELETE'
+      );
+      res.header("X-Total-Count", "1000")
 
 
-//       next();
-//       });    
+      next();
+      });    
       
 server.use("/rickandmorty", router)
 
